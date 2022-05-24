19f897e5317d573e4aa9b94c47847aa280a62f4a
Tornadosan66 <98786639+Tornadosan66@users.noreply.github.com>
2022-05-24 17:04:18 -0500

    Add files via upload

---
+++ storage/framework/views/d04266a010b1ae7eeb5cf4d9bcd8ef5c688ccf21.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Univer</title>
+
+        <link href="css/estilacho.css" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        <h1>Hola mundo</h1>
+    </body>
+</html><?php /**PATH C:\xampp\htdocs\encuesta\resources\views/encuesta.blade.php ENDPATH**/ ?>
